ccbba47641456d97c3dab66d2a1bb6fe530f050e
Donovan <donovan.hoang@viacesi.fr>
2021-03-07 22:10:36 +0100

    add models for Jaune and Kids'

--- app_jaune/models.py
+++ app_jaune/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Kid(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
+class Jaune(models.Model):
+    quantity = models.IntegerField()
+    kid = models.ForeignKey(
+        'Kid',
+        on_delete=models.CASCADE
+    )
